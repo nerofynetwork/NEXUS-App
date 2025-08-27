8e7f486a7ef43b0aad280a176b40d6d74549e868
Blockomega <christian2008@gmx.de>
2025-08-27 20:46:10 +0200

    start thinking about modrinth integration

--- src/main/java/com/zyneonstudios/nexus/application/search/modrinth/ModrinthIntegration.java
+++ src/main/java/com/zyneonstudios/nexus/application/search/modrinth/ModrinthIntegration.java
@@ -1,5 +1,18 @@
 package com.zyneonstudios.nexus.application.search.modrinth;
 
+import com.zyneonstudios.nexus.application.main.NexusApplication;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
 public class ModrinthIntegration {
+
+    public static void install(File installDir, String projectId, String versionId) {
+
+        Path path = Paths.get(NexusApplication.getInstance().getWorkingDir() +"/temp/"+ UUID.randomUUID() +".zip");
+
+    }
 
 }
